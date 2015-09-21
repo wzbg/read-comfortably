67fa8b69699bfadc1b08690c535009c774dd24d3
wzbg <wzbg@github.com>
2015-09-21 23:17:51 +0800

    update images

--- src/readability/grabImages.js
+++ src/readability/grabImages.js
@@ -18,7 +18,6 @@ logger.setLevel('FATAL');
  */
 var fetchUrl = require('fetch').fetchUrl;
 var sizeOf = require('image-size'); // get dimensions of any image file.
-var urlParse = require('url').parse; // Take a URL string, and return an object
 
 /**
  *  grab tht article content's images
@@ -68,15 +67,8 @@ var fetchImage = function (url, images, length, callback) {
       try {
         var dimensions = sizeOf(buf);
         if (dimensions) {
-          var width = image.imgWidth = dimensions.width;
-          var height = image.imgHeight = dimensions.height;
-          var size = calcSize(width, height, MIN_RATE, MAX_RATE); // 计算图片新尺寸
-          var pathname = urlParse(url).pathname;
-          image.name = pathname.substring(pathname.lastIndexOf('/') + 1);
-          image.width = size.width;
-          image.height = size.height;
-          image.x = size.x;
-          image.y = size.y;
+          image.width = dimensions.width;
+          image.height = dimensions.height;
         }
       } catch (e) {
         logger.error('size of[%s] error:', url, e);
@@ -88,24 +80,6 @@ var fetchImage = function (url, images, length, callback) {
       callback(null, images);
     }
   });
-};
-
-var MIN_RATE = 225 / 350; // 最小宽高比
-var MAX_RATE = 225 / 160; // 最大宽高比
-var calcSize = function (imgWidth, imgHeight, minRate, maxRate) { // 计算图片显示尺寸
-  var size = { width: imgWidth, height: imgHeight, x: 0, y: 0, crop: false }; // 原图属性
-  if (imgWidth && imgHeight) {
-    var rate = imgWidth / imgHeight; // 图片实际宽高比
-    if (rate < minRate) { // 小于最小宽高比
-      size.height = imgWidth / minRate; // 截取最大高度
-      size.crop = true; // 需要裁减
-    } else if (rate > maxRate) { // 大于最大宽高比
-      size.width = imgHeight * maxRate; // 截取最大宽度
-      size.x = (imgWidth - size.width) / 2; // 切图位移
-      size.crop = true; // 需要裁减
-    }
-  }
-  return size; // 返回经过计算后的属性
 };
 
 module.exports = grabImages;
