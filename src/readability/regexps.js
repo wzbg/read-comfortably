2a3f58e582b95e5abcd20ac06b1b9c9f1776499e
zyc <z35305515@gmail.com>
2015-11-24 16:54:20 +0800

    techcrunch.com

--- src/readability/regexps.js
+++ src/readability/regexps.js
@@ -2,13 +2,13 @@ module.exports = {
   // 排除候选人
   unlikelyCandidatesRe:    /combx|pager|comment|disqus|foot|header|menu|meta|nav|rss|shoutbox|sidebar|sponsor|share|bookmark|social|advert|leaderboard|instapaper_ignore|entry-unrelated/i,
   // 可能是候选人
-  okMaybeItsACandidateRe:  /and|article|body|column|main|shadow|post|lazy|title|photo/i,
+  okMaybeItsACandidateRe:  /and|article|body|column|main|shadow|post|text|lazy|title|photo/i,
   // 积极
   positiveRe:              /article|body|content|entry|hentry|main|page|pagination|post|text|blog|story/i,
   // 消极
   negativeRe:              /combx|comment|com-|captcha|contact|foot|footer|footnote|masthead|link|media|meta|outbrain|promo|related|scroll|shoutbox|sidebar|sponsor|shopping|utility|tags|tool|widget|tip|dialog/i,
   // 附加排除候选人
-  extraneousRe:            /login|register|signin|signup|related|nothing|button/i,
+  extraneousRe:            /login|register|signin|signup|related|nothing|button|control|more|option/i,
   // div -> p
   divToPElementsRe:        /<(a|blockquote|dl|div|img|ol|p|pre|table|ul)/i,
   // 替换换行标签 ?
